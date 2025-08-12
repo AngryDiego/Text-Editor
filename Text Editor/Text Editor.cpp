e3b1adaaa7707a491b1f08486dd9d924d5c43449
DiegoVS <Diegoanchukov@gmail.com>
2025-08-12 21:12:21 +0200

    Update

---
+++ Text Editor/Text Editor.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <fstream>
+
+class Document {
+    std::string documentName;
+    std::vector <std::string> lines;
+
+public :
+    void AddLine(std::string text) {
+        lines.push_back(text);
+    }
+
+    void DeleteLine(int line) {
+        line--;
+        if (line > lines.size() || line < 0) { return; }
+        else {
+            lines.erase(lines.begin() + line);
+        }
+    }
+
+    void Print() {
+        int index = 0;
+
+        for (auto& line : lines) {
+            std::cout << index + 1 << " : " << line << std::endl;
+            index++;
+        }
+    }
+
+    void LoadFromFile (std::string fName) { //Without .TxT extention
+        std::ifstream froamFile (fName + ".txt");
+        std::string line;
+
+        while (std::getline(froamFile, line)) {
+            lines.push_back(line);
+        }
+    }
+
+    void SaveToFile (std::string fName) { //Without .TxT extention
+        std::ofstream toFile(fName + ".txt");
+        for (auto& line : lines) {
+            toFile << line << std::endl;
+        }
+    }
+
+    Document Clone() { return *this; }
+};
+
+class CommandManager {
+    std::stack <Document> undoStack;
+    std::stack <Document> redoStack;
+
+public:
+    void SaveState(Document& doc) {
+        undoStack.push(doc.Clone());
+        while (!redoStack.empty()) {
+            redoStack.pop();
+        }
+    }
+    bool CanUndo() { return !undoStack.empty(); }
+    bool CanRedo() { return !redoStack.empty(); }
+
+    Document Undo(Document& current) {
+        if (CanUndo) {
+            SaveState(current);
+            Document previous = undoStack.top();
+            undoStack.pop();
+            return previous;
+        }
+        std::cout << "No actions to undo " << std::endl;
+        return current;
+    }
+
+    Document Redo(Document& current) {
+        if (CanRedo) {
+            SaveState(current);
+            Document next = redoStack.top();
+            redoStack.pop();
+            return next;
+        }
+        std::cout << "No actions to redo " << std::endl;
+        return current;
+    }
+};
+
+void ShowMenu() {
+    std::cout << "1.Create new document" << std::endl;
+
+    std::cout << "2.Open document" << std::endl;
+
+    std::cout << "3.Print document" << std::endl;
+
+    std::cout << "4.Add line" << std::endl;
+
+    std::cout << "5.Delete line" << std::endl;
+
+    std::cout << "6.Save to File" << std::endl;
+
+    std::cout << "7.Un do" << std::endl;
+
+    std::cout << "8.Re do" << std::endl;
+
+    std::cout << "0.Exit (you should kill yourself)" << std::endl;
+}
+
+int main () {
+    Document doc;
+
+    CommandManager cmdm;
+
+    int choice = 10;
+
+    while (choice != 0) {
+        ShowMenu();
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            doc = Document();
+            std::cout << "New doc created" << std::endl;
+            break;
+
+        case 2: {
+            std::string fname;
+            std::cout << "Enter file name to load from : ";
+            std::getline(std::cin, fname);
+            doc.LoadFromFile(fname);
+            break;
+        }
+
+        case 3:
+            doc.Print();
+            break;
+
+        case 4: {
+              std::string newline;
+              std::cout << "Enter new line : ";
+              std::getline(std::cin, newline);
+              cmdm.SaveState(doc);
+              doc.AddLine(newline);
+              break;
+        }
+
+        case 5: {
+            int delline;
+            std::cout << "Enter which line you want to delete : ";
+            std::cin >> delline;
+            cmdm.SaveState(doc);
+            doc.DeleteLine(delline);
+            break;
+        }
+
+        case 6: {
+            std::string fname;
+            std::cout << "Enter name of file to save :";
+            std::getline(std::cin, fname);
+            doc.SaveToFile(fname);
+            break;
+        }
+
+        /*case 7:
+            doc = cmdm.Undo(doc);
+            break;
+
+        case 8:
+            doc = cmdm.Redo(doc);
+            break;*/
+
+        case 0: 
+            std::exit(0);
+            break;
+
+        default:
+            break;
+        }
+    }
+}
